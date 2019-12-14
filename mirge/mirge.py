e38ed86dfe81a1bdac693485ca5feca3d2723571
arunhpatil <arun26feb@gmail.com>
2019-12-14 00:34:15 -0500

    reading files

--- mirge/mirge.py
+++ mirge/mirge.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
+#Built-in libraries 
+from pathlib import Path
+import gzip
 
-# Import miRge libraries 
+#Custom miRge libraries 
 from libs.parse import parseArg
 from libs.miRgeEssential import check_dependencies
 
+#with gzip.open('/home/joe/file.txt.gz', 'rb') as f:
+
 args = parseArg()
 check_dependencies(args)
+
+samples = args.samples
+
+print(Path(samples).suffix)
