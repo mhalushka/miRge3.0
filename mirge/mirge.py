bef2de7fdd4fe9c74b0c06db7c48139c20a351b7
arunhpatil <arun26feb@gmail.com>
2019-12-15 15:34:02 -0500

    Added funcion to validate the set of input fastq file paths.

--- mirge/mirge.py
+++ mirge/mirge.py
@@ -11,6 +11,19 @@ from libs.parse import parseArg
 from libs.miRgeEssential import check_dependencies
 
 #with gzip.open('/home/joe/file.txt.gz', 'rb') as f:
+def validate_files(in_fileArray, fastq_fullPath):
+    for files in in_fileArray:
+       filetype = ''.join(Path(files).suffixes) if Path(files).suffix == ".gz" else Path(files).suffix
+       if Path(files).exists() and (filetype == ".fastq" or filetype == ".fastq.gz"):
+           fastq_fullPath.append(str(Path(files).resolve()))
+       else:
+           print(f"\nWARNING: File {files} does not exists!") if not Path(files).exists() else print(f"\nWARNING: File {files} is neither fastq or fastq.gz format!")
+           print(f"Omitting file {files}")
+    #Validating files in the list where the list should not be empty: 
+    if not fastq_fullPath:
+        print("\nERROR!: No valid input files were available!\nPlease verify miRge -s arguments")
+        exit()
+    return fastq_fullPath
 
 def main():
     #WORKING 
@@ -24,27 +37,10 @@ def main():
     fastq_fullPath = [] #Initiating an empty list to collect all the fastq or fastq.gz files based on user arguments!
     if Path(file_list[0]).exists() and Path(file_list[0]).suffix in file_exts: # READ TXT OR CSV FILE HERE
         with open(file_list[0]) as file:
-            for lines in file: 
-                line = lines.strip()
-                filetype = ''.join(Path(line).suffixes) if Path(line).suffix == ".gz" else Path(line).suffix
-                if Path(line).exists() and (filetype == ".fastq" or filetype == ".fastq.gz"):
-                    #print(f"{line} and {filetype}")
-                    fastq_fullPath.append(str(Path(line).resolve()))
-                else:
-                    print(f"\nWARNING: File {line} does not exists!") if not Path(line).exists() else print(f"\nWARNING: File {line} is neither fastq or fastq.gz format!")
-                    print(f"Omitting file {line}")
-    else:
-        for files in file_list: # READ FASTQ OR FASTQ.gz FILES HERE
-            filetype = ''.join(Path(files).suffixes) if Path(files).suffix == ".gz" else Path(files).suffix
-            if Path(files).exists() and (filetype == ".fastq" or filetype == ".fastq.gz"):
-                fastq_fullPath.append(str(Path(files).resolve()))
-            else:
-                print(f"\nWARNING: File {files} does not exists!") if not Path(files).exists() else print(f"\nWARNING: File {files} is neither fastq or fastq.gz format!")
-                print(f"Omitting file {files}")
-    #Validating files in the list where the list should not be empty: 
-    if not fastq_fullPath:
-        print("\nERROR!: No valid input files were available!\nPlease verify miRge -s arguments")
-        exit()
+            lines = [line.strip() for line in file]
+            fastq_fullpath = validate_files(lines, fastq_fullPath)
+    else:  # READ FASTQ OR FASTQ.gz FILES HERE
+        fastq_fullpath = validate_files(file_list, fastq_fullPath)
 
 
 if __name__ == '__main__':
