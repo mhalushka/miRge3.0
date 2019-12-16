bf8bfa1651fb5c1fcf194c057030d28fffb0b4a8
arunhpatil <arun26feb@gmail.com>
2019-12-16 11:29:29 -0500

    Added read directory option

--- mirge/mirge.py
+++ mirge/mirge.py
@@ -22,12 +22,16 @@ def main():
     file_exts = ['.txt', '.csv']
     file_list = samples[0].split(',')
     fastq_fullPath = [] #Initiating an empty list to collect all the fastq or fastq.gz files based on user arguments!
-    if Path(file_list[0]).exists() and Path(file_list[0]).suffix in file_exts: # READ TXT OR CSV FILE HERE
+    if Path(file_list[0]).is_dir():
+        file_list = [str(x) for x in Path(file_list[0]).iterdir() if x.is_file()]
+        fastq_fullpath = validate_files(file_list, fastq_fullPath)
+    elif Path(file_list[0]).exists() and Path(file_list[0]).suffix in file_exts: # READ TXT OR CSV FILE HERE
         with open(file_list[0]) as file:
             lines = [line.strip() for line in file]
             fastq_fullpath = validate_files(lines, fastq_fullPath)
     else:  # READ FASTQ OR FASTQ.gz FILES HERE
         fastq_fullpath = validate_files(file_list, fastq_fullPath)
+    print(fastq_fullPath)
 
 if __name__ == '__main__':
     main()
