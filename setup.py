03ba50bd01aae9a7521174596aa45c7da1c3bea4
arunhpatil <arun26feb@gmail.com>
2020-11-03 18:43:43 -0500

    included manifest file to include files other than .py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
         name='mirge3',
-        version='0.0.1',
+        version='0.0.2',
         author='Arun Patil and Marc Halushka',
         author_email='mhalush1@jhmi.edu',
         url='https://github.com/mhalushka/miRge3.0/',
