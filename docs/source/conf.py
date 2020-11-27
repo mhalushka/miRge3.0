7d3d49c29bf90947e485a4f03aa1fcc20ade7935
Arun Patil <arun26feb@gmail.com>
2020-11-27 17:28:22 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Arun H. Patil and Marc K. Halushka'
 author = 'Arun H. Patil and Marc K. Halushka'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.6'
+release = '0.0.1'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
