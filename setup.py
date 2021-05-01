a4da6f1c2b9748401a2bff106bdae789df7b44b0
arunhpatil <arun26feb@gmail.com>
2021-05-01 17:08:33 -0400

    Corrected MANIFEST.in to include miREC package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setup(
         name='mirge3',
-        version='0.0.8',
+        version='0.0.9',
         author='Arun Patil and Marc Halushka',
         author_email='mhalush1@jhmi.edu',
         url='https://github.com/mhalushka/miRge3.0/',
